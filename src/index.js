adbe1fe5bd73c49356ff165e599c52612407afc4
John Decena <decenajac@gmail.com>
2022-11-16 12:56:59 +0800

    add component files

--- src/index.js
+++ src/index.js
@@ -1,17 +1,73 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { useState, useEffect } from "react";
+import {createRoot} from "react-dom/client";
+import { BrowserRouter, Switch, Routes, Route, Router  } from "react-router-dom";
+import Navbar from "./Navbar.js";
+import Home from "./Home.js";
+import About from "./About.js";
+import Products from "./Products.js";
+import ProductDetails from "./ProductDetails.js";
+import Cart from "./Cart.js";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function App() {
+  const [cart, setCart] = useState([]);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  function handleProductDelete(id) {
+    const updatedCart = cart.filter((product) => product.id !== id);
+    setCart(updatedCart);
+  }
+
+  function handleProductAdd(newProduct) {
+    // check if item exists
+    const existingProduct = cart.find(
+      (product) => product.id === newProduct.id
+    );
+    if (existingProduct) {
+      // increase quantity
+      const updatedCart = cart.map((product) => {
+        if (product.id === newProduct.id) {
+          return {
+            ...product,
+            quantity: product.quantity + 1,
+          };
+        }
+        return product;
+      });
+      setCart(updatedCart);
+    } else {
+      // product is new to the cart
+      setCart([
+        ...cart,
+        {
+          ...newProduct,
+          quantity: 1,
+        },
+      ]);
+    }
+  }
+
+  return (
+    <BrowserRouter>
+      { <Navbar cart={cart} /> }
+      <div className="container">
+    <Routes>
+     
+   
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/about" element={<About />} />
+          <Route exact path="/products" element={<Products
+              cart={cart}
+              onProductAdd={handleProductAdd}
+              onProductDelete={handleProductDelete}
+            />} />
+          <Route  path="/products/:id" element={ <ProductDetails onProductAdd={handleProductAdd} />} /> 
+          <Route exact path="cart" element={ <Cart cart={cart} />} />
+        
+      
+    </Routes>
+    </div>
+  </BrowserRouter>
+  );
+}
+
+createRoot(document.querySelector("#react-root")).render(<App />);
